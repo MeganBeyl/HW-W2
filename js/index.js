c06b4ac30d6ad9109aaf775c8c41afbed9dfe86a
Megan Beyl <231206@virtualwindow.co.za>
2023-05-10 14:41:58 +0200

    Fix exercise 2-4 (calculations)
    Fix percentages and exercise 2's answer.

---
+++ js/index.js
@@ -0,0 +1,62 @@
+var firstName = "Megan";
+let surname = "Beyl";
+let age = 19;
+const subject = "Development";
+
+let message = "Hello, my name is " + firstName + " " + surname + ", I am " + age + " years old and I am studying " + subject;
+
+console.log(message);
+
+let numOne = 5;
+let numTwo = 4;
+let numThree = 3;
+let numFour = 6;
+let answer = 0;
+
+let middle = numTwo / numThree;
+let finalTotal = answer;
+let total = numOne + middle + numFour;
+console.log(answer);
+console.log(total);
+
+let timmy = 10;
+let susan = 25;
+let timmyTax = 15/100;
+let susanTax = 26/100;
+
+let tValue = 1.5;
+let sValue = 6.5;
+let value = tValue + sValue;
+
+let numMessage = "The Tax Man will claim " + value + "% from Timmy and Susan";
+
+console.log(numMessage);
+
+let john = 50;
+let tim = 56;
+let alfie = 74;
+let gwen = 37;
+
+let students = john + tim + alfie + gwen;
+let testTotal = 100 + 100 + 100 + 100;
+let totalOverall = students/testTotal;
+let percentage = 54.25;
+
+let statement = "The students average is: " + percentage + "/100";
+
+console.log(statement);
+
+let updatedJohn = 57;
+let updatedTim = 63;
+let updatedAlfie = 81;
+let updatedGwen = 44;
+
+let updated = updatedJohn + updatedTim + updatedAlfie + updatedGwen;
+let updatedAvg = updated/400;
+let updatedPercentage = 61.25;
+
+let updatedStatement = "The students updated average is: " + updatedPercentage + "/100";
+
+console.log(updatedStatement);
+
+
